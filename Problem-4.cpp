977d225e3f770eb617b6e1dc942658803d2e1d35
Rahulkumar7818 <rahul.2206232@gmail.com>
2025-12-19 12:16:12 +0530

    Count numbers divisible by 1 to 9 from a given list
    Updated the program to include comments explaining how to make the array input flexible.

--- Problem-4.cpp
+++ Problem-4.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 
 int main() {
-    vector<int> arr= {1,2,8,9,12,46,76,82,15,20,30};
+    
+    // Instead of hardcoding the array, we can also take the size of the array
+    // and its elements as input from the user using cin.
+    // This makes the program more flexible for different inputs.
+    vector<int> arr = {1,2,8,9,12,46,76,82,15,20,30};
 
-    for (int i= 1; i<= 9; i++) {
+    for (int i = 1; i <= 9; i++) {
         int count = 0;
-        for (int j=0;j<arr.size();j++) {
-            if (arr[j]% i == 0)
+        for (int j = 0; j < arr.size(); j++) {
+             int x=arr[j]%i;
+            if (x == 0)
                 count++;
         }
         cout << i << ": " << count << endl;
     }
     return 0;
 }
-
